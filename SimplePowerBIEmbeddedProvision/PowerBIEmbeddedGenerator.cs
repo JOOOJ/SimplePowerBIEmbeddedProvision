c0252633ca75893752cc582613dedacdeb2b58b7
JOOOJ <jjjrachel@hotmail.com>
2016-08-03 17:03:15 +0800

    Add PowerBIEmbeddedGenerator class

---
+++ SimplePowerBIEmbeddedProvision/PowerBIEmbeddedGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.PowerBI.Api.V1;
+using System.Configuration;
+
+
+namespace SimplePowerBIEmbeddedProvision
+{
+    internal class PowerBIEmbeddedGenerator
+    {
+        static string AccessKey = ConfigurationManager.AppSettings["AccessKey"];
+        static string WorkspaceCollectionName = ConfigurationManager.AppSettings["WorkspaceCollectionName"];
+        static string WorkspaceId = ConfigurationManager.AppSettings["WorkspaceId"];
+        private async Task<IPowerBIClient> CreateClient()
+        {
+            return null;
+        }
+
+        public async Task<Workspaces> CreateWorkspace()
+        {
+            return null;
+        }
+
+        public async Task<Imports> UploadPBIXFile()
+        {
+            return null;
+        }
+    }
+}
--- SimplePowerBIEmbeddedProvision/Program.cs
+++ SimplePowerBIEmbeddedProvision/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.PowerBI.Api.V1;
 
 namespace SimplePowerBIEmbeddedProvision
 {
@@ -10,6 +11,44 @@ namespace SimplePowerBIEmbeddedProvision
     {
         static void Main(string[] args)
         {
+            Run().GetAwaiter().GetResult();            
+        }       
+
+        static async Task Run()
+        {
+            Console.WriteLine("Please input 1 or 2 to choose step 1 or step 2 to provision Power BI Embedded:");
+            Console.WriteLine("1. Provision a new workspace in an existing workspace collection");
+            Console.WriteLine("2. Import PBIX Desktop file into an existing workspace");
+            Console.WriteLine("3. Import PBIX Desktop file into a new workspace");
+            Console.WriteLine();
+
+            var key = Console.ReadKey(true);
+            bool exit = false;
+            PowerBIEmbeddedGenerator powerBI = new PowerBIEmbeddedGenerator();
+            switch (key.KeyChar)
+            {
+                case '1':
+                    await powerBI.CreateWorkspace();
+                    await Run();
+                    break;
+                case '2':                    
+                    await powerBI.UploadPBIXFile();
+                    await Run();
+                    break;
+                case '3':
+                    await powerBI.CreateWorkspace();
+                    await powerBI.UploadPBIXFile();
+                    await Run();
+                    break;
+                default:
+                    exit = true;
+                    Console.WriteLine("Please enter any key to exit...");
+                    break;
+            }
+            if (!exit)
+            {
+                await Run();
+            }
         }
     }
 }
