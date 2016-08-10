7a3b2d234a150244c80504fa885437f6e1ea5c7e
JOOOJ <jjjrachel@hotmail.com>
2016-08-10 18:19:57 +0800

    Complete CreateWorkspace API

--- PowerBIEmbeddedLib/PowerBIDeploy.cs
+++ PowerBIEmbeddedLib/PowerBIDeploy.cs
@@ -16,7 +16,17 @@ namespace PowerBIEmbeddedLib
     public class PowerBIDeploy
     {
         const string powerBIApiEndpoint = "https://api.powerbi.com";
-        
+        public Workspace CreateWorkspace(string workspaceCollectionName, string accessToken)
+        {
+            if (string.IsNullOrEmpty(workspaceCollectionName) || string.IsNullOrEmpty(accessToken))
+            {
+                throw new ArgumentNullException("workspaceCollectionName or accessToken", "The parameters are null or empty.");
+            }
+            using (IPowerBIClient client = CreateClient(accessToken))
+            {
+                return client.Workspaces.PostWorkspace(workspaceCollectionName);
+            }
+        }
         private IPowerBIClient CreateClient(string accessToken)
         {
             TokenCredentials token = new TokenCredentials(accessToken, "AppKey");
