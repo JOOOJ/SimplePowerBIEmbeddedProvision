3d1b40bbbf00d979a66e37a8c722a7c7a234d4c5
JOOOJ <jjjrachel@hotmail.com>
2016-08-11 14:49:51 +0800

    Add interface RetrieveAllWorkspaceByCollectionName and UploadPbixSingleFile, and one basic function CheckParameters

--- PowerBIEmbeddedLib/PowerBIDeploy.cs
+++ PowerBIEmbeddedLib/PowerBIDeploy.cs
@@ -10,6 +10,7 @@ using System.Net.Http.Headers;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System.Net;
 using Microsoft.Rest;
+using System.IO;
 
 namespace PowerBIEmbeddedLib
 {
@@ -18,21 +19,56 @@ namespace PowerBIEmbeddedLib
         const string powerBIApiEndpoint = "https://api.powerbi.com";
         public Workspace CreateWorkspace(string workspaceCollectionName, string accessToken)
         {
-            if (string.IsNullOrEmpty(workspaceCollectionName) || string.IsNullOrEmpty(accessToken))
-            {
-                throw new ArgumentNullException("workspaceCollectionName or accessToken", "The parameters are null or empty.");
-            }
+            CheckParameters(workspaceCollectionName, accessToken);
             using (IPowerBIClient client = CreateClient(accessToken))
             {
                 return client.Workspaces.PostWorkspace(workspaceCollectionName);
             }
         }
+
+        public IList<Workspace> RetrieveAllWorkspaceByCollectionName(string workspaceCollectionName, string accessToken)
+        {
+            CheckParameters(workspaceCollectionName, accessToken);
+            using (IPowerBIClient client = CreateClient(accessToken))
+            {
+                return client.Workspaces.GetWorkspacesByCollectionName(workspaceCollectionName).Value;
+            }
+        }
+
+        public Import UploadPbixSingleFile(string workspaceCollectionName, string filePath, string reportName, string accessToken, string workspaceId)
+        {
+            CheckParameters(workspaceCollectionName, filePath, reportName, accessToken, workspaceId);
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("The file cannot be found, please input correct file path", filePath);
+            }
+            using (FileStream stream = File.Open(filePath, FileMode.Open))
+            {
+                using (IPowerBIClient client = CreateClient(accessToken))
+                {
+                    return client.Imports.PostImportWithFile(workspaceCollectionName, workspaceId, stream);
+                }
+            }
+        }
+         
         private IPowerBIClient CreateClient(string accessToken)
         {
+            CheckParameters(accessToken);
             TokenCredentials token = new TokenCredentials(accessToken, "AppKey");
             IPowerBIClient client = new PowerBIClient(token);
             client.BaseUri = new Uri(powerBIApiEndpoint);
             return client;
+        }
+
+        private void CheckParameters(params string[] parameters)
+        {
+            foreach (string item in parameters)
+            {
+                if (string.IsNullOrEmpty(item))
+                {
+                    throw new ArgumentNullException(item, string.Format("{0} is incorrect", item));
+                }
+            }
         }
     }
 }
