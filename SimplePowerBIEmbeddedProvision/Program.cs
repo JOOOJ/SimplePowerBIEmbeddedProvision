01a6e9ab6e5f892a1f2b944cb7108b8977a85389
JOOOJ <jjjrachel@hotmail.com>
2016-08-17 11:54:59 +0800

    Update behavior if no dataset exists in the workspace collections

--- SimplePowerBIEmbeddedProvision/Program.cs
+++ SimplePowerBIEmbeddedProvision/Program.cs
@@ -12,12 +12,12 @@ namespace SimplePowerBIEmbeddedProvision
     {
         static void Main(string[] args)
         {
-            Run().GetAwaiter().GetResult();            
-        }       
+            Run().GetAwaiter().GetResult();
+        }
 
         static async Task Run()
         {
-            Console.WriteLine("Please input 1 or 2 to choose step 1 or step 2 to provision Power BI Embedded:");
+            Console.WriteLine("Please input choose number, such as 1 or 2 to choose steps to provision Power BI Embedded:");
             Console.WriteLine("1. Provision a new workspace in an existing workspace collection");
             Console.WriteLine("2. Import PBIX Desktop file into an existing workspace");
             Console.WriteLine("3. Import PBIX Desktop file into a new workspace");
@@ -31,14 +31,14 @@ namespace SimplePowerBIEmbeddedProvision
             {
                 case '1':
                     Workspace workspace = await powerBI.CreateWorkspace();
-                    if(workspace==null)
+                    if (workspace == null)
                     {
                         Console.WriteLine("Create workspace failed.");
                         exit = true;
                         break;
                     }
-                    
-                    Console.WriteLine("Workspace id:{0}",workspace.WorkspaceId);
+
+                    Console.WriteLine("Workspace id:{0}", workspace.WorkspaceId);
                     await Run();
                     break;
                 case '2':
@@ -47,7 +47,7 @@ namespace SimplePowerBIEmbeddedProvision
                     Console.WriteLine("Please create a report name, e.g. 'myreport':");
                     string dataset = Console.ReadLine();
                     Import import = await powerBI.UploadPBIXFile(filepath, dataset);
-                    Console.WriteLine("The file is imported, and the id is {0}",import.Id);
+                    Console.WriteLine("The file is imported, and the id is {0}", import.Id);
                     Console.WriteLine();
                     await Run();
                     break;
@@ -64,18 +64,27 @@ namespace SimplePowerBIEmbeddedProvision
                     break;
                 case '4':
                     Dictionary<string, string> dict = await powerBI.GetAllResports();
-                    Console.WriteLine("The below is all reports, please choose correct report id you want to delete.");
-                    foreach (KeyValuePair<string,string> item in dict)
+                    if (dict.Count > 0)
                     {
-                        Console.WriteLine("Workspace Id {0}",item.Key);
-                        Console.WriteLine("   {0}",item.Value);
+                        Console.WriteLine("The below is all reports, please choose correct report id you want to delete.");
+                        foreach (KeyValuePair<string, string> item in dict)
+                        {
+                            Console.WriteLine("Workspace Id {0}", item.Key);
+                            Console.WriteLine("   {0}", item.Value);
+                        }
+                        Console.WriteLine("Please input a workspace id:");
+                        string workspaceId = Console.ReadLine();
+                        Console.WriteLine("Please input a report id you want to delete:");
+                        string reportId = Console.ReadLine();
+                        await powerBI.DeleteReport(reportId, workspaceId);
+                        Console.WriteLine("Dataset deleted successfully.");
                     }
-                    Console.WriteLine("Please input a workspace id:");
-                    string workspaceId = Console.ReadLine();
-                    Console.WriteLine("Please input a report id you want to delete:");
-                    string reportId = Console.ReadLine();
-                    await powerBI.DeleteReport(reportId,workspaceId);
-                    Console.WriteLine("Dataset deleted successfully.");
+                    else
+                    {
+                        Console.WriteLine("There is no any report exist on this workspace collection.");
+                        Console.WriteLine();
+                        await Run();
+                    }
                     break;
                 default:
                     exit = true;
